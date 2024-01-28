343b7482e0377e7ba34b297a0b5ad77ee3ef82be
SuryaiConnect <suryakant.chavan@iconnectsolutions.com>
2024-01-28 12:02:56 +0530

    Header Navigation Desktop view

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,34 @@
-console.log('Hello World');
+$(document).ready(function(){
+    /* Close Menu */
+    function closeMenu(){
+        $('.headerWrapper').removeClass('open');
+        $('.row').removeClass('open');
+        $('.mainNav__item-link').removeClass('active'); 
+    }
+
+    /* Open Menu */
+    function openMenu(menuItemId){
+        $('.headerWrapper ').addClass('open');
+        $('#'+menuItem).addClass('open');
+    }
+
+    $('.mainNav__item-link').click(function(){
+        
+        menuItem = $(this).children('.p__small').text().toLowerCase();
+        menuItem = menuItem.replace(/\s+/g, '-').trim();                
+
+        if($("#" + menuItem).length != 0) {
+            // Submenu Exist                    
+            if($(this).hasClass('active')){
+                $(this).removeClass('active');
+                closeMenu();
+            }else{
+                closeMenu();
+                $(this).addClass('active');
+                openMenu(menuItem);
+            }
+        }else{
+            closeMenu();                    
+        }
+    })
+});
