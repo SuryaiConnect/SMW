572954b4019ce0a4c0583a8ba8e28fe68141436f
SuryaiConnect <suryakant.chavan@iconnectsolutions.com>
2024-01-30 13:20:46 +0530

    Main Navigation

--- src/js/header.js
+++ src/js/header.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
     /* Close Menu */
     function closeMenu(){
         $('.headerWrapper').removeClass('open');
-        $('.row').removeClass('open');
-        $('.mainNav__item-link').removeClass('active'); 
+        $('.mainNav__item-link').removeClass('active');
     }
 
     /* Open Menu */
     function openMenu(menuItemId){
-        $('.headerWrapper ').addClass('open');
+        // $('.headerWrapper ').addClass('open');
         $('#'+menuItem).addClass('open');
     }
 
+    /* mainNav open-close */
     $('.mainNav__item-link').click(function(){
         
         menuItem = $(this).children('.p__small').text().toLowerCase();
@@ -31,4 +31,34 @@ $(document).ready(function(){
             closeMenu();                    
         }
     })
+
+    /* 
+    * Mobile
+    */
+
+    /* Open Main Nav */
+    $('#m-burger').click(function(){
+        $('.header').addClass('slide-open'); 
+    })
+    
+    /* Close Main Nav */
+    $('#close-mainNav').click(function(){
+        $('.header').removeClass('slide-open'); 
+    })
+    
+    /* submenu back */
+    $('.submenu-back').click(function(){
+        $('.headerWrapper').removeClass('open'); 
+        $('.mainNav__item-link').removeClass('active'); 
+    })
+
+    /* close submenu - main menu (close all) */
+    $('.close-subNav').click(function(){
+        /* submenu close */
+        $('.headerWrapper').removeClass('open');
+        $('.mainNav__item-link').removeClass('active');
+        /* mainNav mobile close */
+        $('.header').removeClass('slide-open'); 
+    });
+    
 });
