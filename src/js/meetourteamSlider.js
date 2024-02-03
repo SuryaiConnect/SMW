954ba0f3afc7f80701e665c3f67b2f6fc02753f6
SuryaiConnect <suryakant.chavan@iconnectsolutions.com>
2024-02-03 18:38:40 +0530

    Main Navigation fixes and About Us page created

---
+++ src/js/meetourteamSlider.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    /* Meet Our Team Slider */
+    $('#meetourTeamSlider').owlCarousel({
+        loop:true,
+        margin:32,
+        stagePadding: 2,
+        nav:true,
+        responsiveClass: true,        
+        dots: false,
+        navText : ["<i class='smw-icon-button-left'></i>","<i class='smw-icon-button-right'></i>"],
+        responsive:{
+            0:{
+                items:1.2,
+                center: true,
+                dots: false,
+                nav: false
+            },
+            567:{
+                items:2,
+                nav:false,
+                dots: false,
+                center: true,
+            },
+            768:{
+                items:2,
+                nav:false, 
+                dots: false,
+                center: true,
+            },
+            991:{
+                items:3,
+                nav:true,
+                center: false, 
+                dots: false, 
+            },
+            1140:{
+                items:4,
+                nav:true,
+                center: false, 
+                dots: false, 
+            }
+        }
+    });
+});
