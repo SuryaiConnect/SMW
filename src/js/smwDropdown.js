3bac510609e384e492f756715a51f40d7e6a282e
SuryaiConnect <suryakant.chavan@iconnectsolutions.com>
2024-02-04 15:21:45 +0530

    Dropdown select text change logic added

---
+++ src/js/smwDropdown.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.smwFilter .dropdown-menu a').on('click', function(){
+        var selectedText = $(this).text();
+        $(this).closest('.dropdown').find('.smwdropdown__text').text(selectedText);
+    });
+});
