5a20ca2c622b1e9c747ebeaceb2f6c0936f5c5da
SuryaiConnect <suryakant.chavan@iconnectsolutions.com>
2024-02-15 10:49:54 +0530

    Expertise page design

--- src/js/relatedExpertiseSlider.js
+++ src/js/relatedExpertiseSlider.js
@@ -5,34 +5,41 @@ $(document).ready(function(){
 
     let $relatedExpertiseSlider = $('.relatedExpertiseSlider');
     if ($relatedExpertiseSlider.length > 0) {
-        $($relatedExpertiseSlider).owlCarousel({
-            loop:true,
-            margin:72,
-            // stagePadding: 2,
-            nav:true,
-            responsiveClass: true,        
-            dots: true,
-            navText : ["<i class='smw-icon-button-left'></i>","<i class='smw-icon-button-right'></i>"],
-            responsive:{
-                0:{
-                    items:1.2,
-                    center: true,
-                    dots: true,
-                    nav: true,
-                },
-                768:{
-                    items:2,
-                    nav:true,
-                    dots: true,
-                    center: true,
-                },
-                991:{
-                    items:3,
-                    nav:true,
-                    center: true, 
-                    dots: false, 
+        if ($(window).width() >= 768) {
+            $($relatedExpertiseSlider).owlCarousel({
+                loop:true,
+                margin:72,
+                nav:true,
+                responsiveClass: true,        
+                dots: true,
+                navText : ["<i class='smw-icon-button-left'></i>","<i class='smw-icon-button-right'></i>"],
+                responsive:{
+                    0:{
+                        items:2,
+                        center: false,
+                        dots: false,
+                        nav: true,
+                    },
+                    768:{
+                        items:2,
+                        nav:true,
+                        dots: false,
+                        center: false,
+                    },
+                    991:{
+                        items:3,
+                        nav:true,
+                        center: false, 
+                        dots: false, 
+                    }
                 }
-            }
-        })
+            })
+        }else{            
+            // Destroy the Owl Carousel instance if it's in mobile view
+            // On expertise page
+            $($relatedExpertiseSlider).trigger('destroy.owl.carousel').removeClass('owl-loaded');
+            $($relatedExpertiseSlider).find('.owl-stage-outer').children().unwrap(); 
+            $($relatedExpertiseSlider).addClass('d-flex flex-column gap-49'); 
+        }
     }
 });
